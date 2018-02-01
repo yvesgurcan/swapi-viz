85e0b3608a26ed285ea2a6437f23ae8b195a21e2
Yves <gurcan.yves+github@gmail.com>
2018-01-31 20:32:02 -0800

    new interface

--- Main.elm
+++ Main.elm
@@ -4,128 +4,56 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import RemoteData exposing (..)
-import Json.Decode as JD
+import Json.Decode as Decode
 
-
-swapi : String
-swapi =
+api : String
+api =
     "https://swapi.co/api/"
 
-
-
-{- Model
-   the state of your application
--}
-
-
-type alias Model =
-    { planets : WebData (List Planet) }
-
-
-type alias Planet =
-    { name : String
-    , diameter : String
+main =
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
     }
 
+-- MODEL
 
-initModel : Model
-initModel =
-    { planets = Loading }
+type alias Model =
+    { planetName : String
+    , api : String}
 
+type Msg = SetPlanetName String
 
-type Msg
-    = PlanetsResp (WebData (List Planet))
+model : Model
+model =
+    {planetName = "Endor"
+    , api = api}
 
+-- UPDATE
 
-planetsDecoder : JD.Decoder (List Planet)
-planetsDecoder =
-    JD.at [ "results" ] <|
-        JD.list <|
-            JD.map2 Planet
-                (JD.field "name" JD.string)
-                (JD.field "diameter" JD.string)
+update : Msg -> Model -> Model
+update msg model = 
+    case msg of 
+        SetPlanetName string -> 
+            { model | planetName = string }
 
-
-getPlanets : Cmd Msg
-getPlanets =
-    Http.get (swapi ++ "planets") planetsDecoder
-        |> RemoteData.sendRequest
-        |> Cmd.map PlanetsResp
-
-
-
-{- Update
-   a way to update your state
--}
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        PlanetsResp planets ->
-            ( { model | planets = planets }, Cmd.none )
-
-
-
-{- View
-   a way to view your state as HTML
--}
-
+-- VIEW
 
 view : Model -> Html Msg
 view model =
-    case model.planets of
-        Success planets ->
-            div [] (List.map planetView planets)
-
-        Loading ->
-            h1 [] [ text "Loading..." ]
-
-        _ ->
-            text <| toString model
-
-
-planetView : Planet -> Html Msg
-planetView planet =
-    let
-        width =
-            String.toFloat planet.diameter |> Result.map ((*) 0.01)
-
-        widthPx =
-            case width of
-                Ok w ->
-                    (toString w) ++ "px"
-
-                _ ->
-                    "0px"
-    in
-        div
-            [ class "planet-wrap", style [ ( "width", widthPx ) ] ]
-            [ div
-                [ class "planet"
-                , style
-                    [ ( "width", widthPx )
-                    , ( "height", widthPx )
-                    , ( "background-image", "-webkit-radial-gradient(45px 45px, circle cover, pink, purple)" )
-                    ]
-                ]
-                []
-            , h3
-                []
-                [ text planet.name ]
+    div
+        [ style
+            [ ( "width", "85vw" )
+            , ( "height", "85vh" )
+            , ( "padding", "20px" )
+            , ( "margin", "20px" )
+            , ( "border", "1px solid black" )
+            , ( "border-radius", "15px" )
             ]
-
-
-
-{- Tie it all together -}
-
-
-main : Program Never Model Msg
-main =
-    program
-        { update = update
-        , view = view
-        , init = ( initModel, getPlanets )
-        , subscriptions = always Sub.none
-        }
+        ]
+        [ h1 [ style [ ( "text-align", "center" ) ] ] [ text "Welcome to the Star Wars Universe" ]
+        , div [] [ label [] [text "Enter the name of a planet"] ]
+        , input [ placeholder "Tatooine", onInput SetPlanetName, value model.planetName ] []
+        , div [] []
+        ]
